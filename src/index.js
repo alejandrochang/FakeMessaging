a0f5bf472186d335d907d313886aa11a9446fe30
Alejandro <alejandrochang10@gmail.com>
2018-12-05 15:41:42 -0800

    diving into nesting, reusability and configuration

--- src/index.js
+++ src/index.js
@@ -2,12 +2,22 @@
 // Create a React Component
 // Take the React Component and show it on the screen
 
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
 // function based component
+
 const App = () => {
-    return <div>Hi There</div>;
+    const buttonText = { text: 'Click on Me!' };
+    const style = { backgroundColor: 'blue', color: 'white' };
+
+    return (
+        <div>
+            <label className="label" htmlFor="name">Enter name: </label>
+            <input id="name" type="text"/>
+            <button style={style}> { buttonText.text } </button>
+        </div>
+    );
 };
 
 ReactDOM.render(<App />, document.querySelector("#root"));
