7f5160cc6366107a84629367e9b540078cd50e1e
Alejandro <alejandrochang10@gmail.com>
2018-12-05 22:51:09 -0800

    messages and approvalCards components

--- src/index.js
+++ src/index.js
@@ -1,53 +1,53 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import faker from 'faker';
+import React from "react";
+import ReactDOM from "react-dom";
+import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
+import faker from "faker";
 
 const App = () => {
+  const comments = [
+    "hello there",
+    "how're you doing",
+    "long night",
+    "LOL",
+    "OMG",
+    ":D woooowww!"
+  ];
 
-	return <div className="ui container comments">
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.avatar()}/>
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Sam
-          </a>
-					<div className="meta-data">
-						<span className="date"> Today at 6:00PM</span>
-					</div>
-					<div className="text">Nice blogpost!</div>
-        </div>
-      </div>
-			<div className="comment">
-				<a href="/" className="avatar">
-					<img alt="avatar" src={faker.image.avatar()} />
-				</a>
-				<div className="content">
-					<a href="/" className="author">
-						Sam
-						</a>
-					<div className="meta-data">
-						<span className="date"> Today at 6:00PM</span>
-					</div>
-					<div className="text">Nice blogpost!</div>
-				</div>
-			</div>
-			<div className="comment">
-				<a href="/" className="avatar">
-					<img alt="avatar" src={faker.image.avatar()} />
-				</a>
-				<div className="content">
-					<a href="/" className="author">
-						Sam
-						</a>
-					<div className="meta-data">
-						<span className="date"> Today at 6:00PM</span>
-					</div>
-					<div className="text">Nice blogpost!</div>
-				</div>
-			</div>
-    </div>;
-}
+  return (
+    <div className="ui container comments">
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          commentDate="Today at 5:00 PM"
+          image={faker.image.avatar()}
+          comment={comments[Math.floor(Math.random() * comments.length)]}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Jorge"
+          commentDate="Monday at 12:35AM"
+          image={faker.image.avatar()}
+          comment={comments[Math.floor(Math.random() * comments.length)]}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Ryan"
+          commentDate="Oct 25, 2018 at 3:02PM"
+          image={faker.image.avatar()}
+          comment={comments[Math.floor(Math.random() * comments.length)]}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail author="Elizabeth" commentDate="Yesterday at 1:00PM"
+        image={faker.image.avatar()}
+        comment={comments[Math.floor(Math.random() * comments.length)]}
+        />
+      </ApprovalCard>
+    </div>
+  );
+};
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector("#root"));
